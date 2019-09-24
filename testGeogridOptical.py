607237459684fd675ed0abdda4a41cfd898a92dc
Yang Lei <leiyangfrancis@gmail.com>
2019-09-24 16:28:57 -0700

    Add files via upload

---
+++ testGeogridOptical.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Copyright 2019 California Institute of Technology. ALL RIGHTS RESERVED.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# United States Government Sponsorship acknowledged. This software is subject to
+# U.S. export control laws and regulations and has been classified as 'EAR99 NLR'
+# (No [Export] License Required except when exporting to an embargoed country,
+# end user, or in support of a prohibited end use). By downloading this software,
+# the user agrees to comply with all applicable U.S. export laws and regulations.
+# The user has the responsibility to obtain export licenses, or other export
+# authority as may be required before exporting this software to any 'EAR99'
+# embargoed foreign country or citizen of those countries.
+#
+# Authors: Piyush Agram, Yang Lei
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+def cmdLineParse():
+    '''
+    Command line parser.
+    '''
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Output geo grid')
+    parser.add_argument('-m', '--input_m', dest='indir_m', type=str, required=True,
+            help='Input folder with ISCE swath files for master image')
+    parser.add_argument('-s', '--input_s', dest='indir_s', type=str, required=True,
+            help='Input folder with ISCE swath files for slave image')
+#    parser.add_argument('-o', '--output', dest='outfile', type=str, default='geogrid.csv',
+#            help='Output grid mapping')
+    parser.add_argument('-d', '--dem', dest='demfile', type=str, required=True,
+            help='Input DEM')
+    parser.add_argument('-sx', '--dhdx', dest='dhdxfile', type=str, default="",
+            help='Input slope in X')
+    parser.add_argument('-sy', '--dhdy', dest='dhdyfile', type=str, default="",
+            help='Input slope in Y')
+    parser.add_argument('-vx', '--vx', dest='vxfile', type=str, default="",
+            help='Input velocity in X')
+    parser.add_argument('-vy', '--vy', dest='vyfile', type=str, default="",
+            help='Input velocity in Y')
+
+    return parser.parse_args()
+
+class Dummy(object):
+    pass
+
+
+
+
+
+def loadMetadata(indir):
+    '''
+    Input file.
+    '''
+    import os
+    import numpy as np
+
+    from osgeo import gdal, osr
+    import struct
+        
+    DS = gdal.Open(indir, gdal.GA_ReadOnly)
+    trans = DS.GetGeoTransform()
+
+    info = Dummy()
+    info.startingX = trans[0]
+    info.startingY = trans[3]
+    info.XSize = trans[1]
+    info.YSize = trans[5]
+    
+    nameString = os.path.basename(DS.GetDescription())
+    info.time = float(nameString.split('_')[3])
+
+    info.numberOfLines = DS.RasterYSize
+    info.numberOfSamples = DS.RasterXSize
+    
+    info.filename = indir
+
+
+    return info
+
+def runGeogrid(info, info1, dem, dhdx, dhdy, vx, vy):
+    '''
+    Wire and run geogrid.
+    '''
+    import GeogridOptical as GO
+#    from components.contrib.geo_autoRIFT.geogrid import GeogridOptical
+
+    obj = GO.GeogridOptical()
+#    obj = GeogridOptical()
+
+    obj.startingX = info.startingX
+    obj.startingY = info.startingY
+    obj.XSize = info.XSize
+    obj.YSize = info.YSize
+    obj.repeatTime = (info1.time - info.time) * 24.0 * 3600.0
+    obj.numberOfLines = info.numberOfLines
+    obj.numberOfSamples = info.numberOfSamples
+    
+    obj.dat1name = info.filename
+    obj.demname = dem
+    obj.dhdxname = dhdx
+    obj.dhdyname = dhdy
+    obj.vxname = vx
+    obj.vyname = vy
+    obj.winlocname = "window_location.tif"
+    obj.winoffname = "window_offset.tif"
+    obj.winro2vxname = "window_rdr_off2vel_x_vec.tif"
+    obj.winro2vyname = "window_rdr_off2vel_y_vec.tif"
+
+    obj.runGeogrid()
+
+
+if __name__ == '__main__':
+    '''
+    Main driver.
+    '''
+
+    inps = cmdLineParse()
+
+    metadata_m = loadMetadata(inps.indir_m)
+    metadata_s = loadMetadata(inps.indir_s)
+
+    runGeogrid(metadata_m, metadata_s, inps.demfile, inps.dhdxfile, inps.dhdyfile, inps.vxfile, inps.vyfile)
