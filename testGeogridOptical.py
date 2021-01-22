74c85ce1c6a263a373fc6bce917f131f8d0fcf78
Joseph H Kennedy <me@jhkennedy.org>
2021-01-22 14:20:31 -0900

    Update Lansat-8 search

--- testGeogridOptical.py
+++ testGeogridOptical.py
@@ -95,7 +95,7 @@ def loadMetadata(indir):
     info.XSize = trans[1]
     info.YSize = trans[5]
 
-    if re.findall("L.08_",DS.GetDescription()).__len__() > 0:
+    if re.findall("L[CO]08_",DS.GetDescription()).__len__() > 0:
         nameString = os.path.basename(DS.GetDescription())
         info.time = nameString.split('_')[3]
     elif re.findall("S2._",DS.GetDescription()).__len__() > 0:
@@ -138,7 +138,7 @@ def coregisterLoadMetadata(indir_m, indir_s):
     info.XSize = trans[1]
     info.YSize = trans[5]
 
-    if re.findall("L.08_",DS.GetDescription()).__len__() > 0:
+    if re.findall("L[CO]08_",DS.GetDescription()).__len__() > 0:
         nameString = os.path.basename(DS.GetDescription())
         info.time = nameString.split('_')[3]
     elif re.findall("S2._",DS.GetDescription()).__len__() > 0:
@@ -155,7 +155,7 @@ def coregisterLoadMetadata(indir_m, indir_s):
 
     info1 = Dummy()
 
-    if re.findall("L.08_",DS1.GetDescription()).__len__() > 0:
+    if re.findall("L[CO]08_",DS1.GetDescription()).__len__() > 0:
         nameString1 = os.path.basename(DS1.GetDescription())
         info1.time = nameString1.split('_')[3]
     elif re.findall("S2._",DS1.GetDescription()).__len__() > 0:
